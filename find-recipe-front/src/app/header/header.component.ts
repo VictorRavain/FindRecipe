2dddbf75d5939fe9265f60ebe73769e7666128ed
Victor <victorravain@hotmail.fr>
2024-10-11 17:53:17 +0200

    Connexion and Inscription front dev

--- find-recipe-front/src/app/header/header.component.ts
+++ find-recipe-front/src/app/header/header.component.ts
@@ -1,16 +1,35 @@
 import { Component, HostListener  } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { loginComponent } from '../login/login.component';
+import { InscriptionComponent } from '../inscription/inscription.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, loginComponent,InscriptionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   isOpen: boolean = false;  // Controls whether the burger menu is open
   isMobileView: boolean = false;  // Controls when to show the burger icon
+  isLoginModalOpen = false; 
+  isSignupModalOpen = false;
+
+  openLoginModal() {
+    this.isLoginModalOpen = true;
+    this.isSignupModalOpen = false; // Close signup modal if it's open
+  }
+
+  openSignupModal() {
+    this.isSignupModalOpen = true;
+    this.isLoginModalOpen = false; // Close login modal if it's open
+  }
+
+  closeModal() {
+    this.isLoginModalOpen = false;
+    this.isSignupModalOpen = false;
+  }
 
   constructor() {
     this.checkWindowWidth(); // Check initial window width
