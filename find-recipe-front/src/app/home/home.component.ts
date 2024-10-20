7656ebc3465846b0437b0017e27d778b1e507b61
Victor <victorravain@hotmail.fr>
2024-10-20 20:22:26 +0200

    Home page Front dev

--- find-recipe-front/src/app/home/home.component.ts
+++ find-recipe-front/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isMobileView: boolean = false;  // Controls when to show the burger icon
+  selectedOption: string = 'Ingrédients';  // Default selected 
+  alternativeOption: string = 'Recettes';  // Default alternative option
+  isDropdownOpen: boolean = false;
+  placeholder: string = "Ajoutez un ingrédient";
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowWidth(); // Update on window resize
+  }
+
+  // Closes the dropdown when clicking outside
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    // Close the dropdown if the click is outside of the button or dropdown
+    if (!targetElement.closest('.button-container') && !targetElement.closest('.select-menu')) {
+      this.isDropdownOpen = false;
+    }
+  }
+
+  // (Optional) Close the dropdown if the window loses focus
+  @HostListener('window:blur')
+  onWindowBlur() {
+    this.isDropdownOpen = false;
+  }
+
+  // Toggles the dropdown visibility
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // Sets the selected option and updates the alternative option
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.placeholder = option === "Ingrédients" ? "Ajoutez un ingrédient" :" Rechercher une recette";
+    this.alternativeOption = option === 'Ingrédients' ? 'Recettes' : 'Ingrédients';
+    this.isDropdownOpen = false;
+  }
+
+  // Function to check window width and determine mobile view
+  private checkWindowWidth() {
+    this.isMobileView = window.innerWidth < 768; // Adjust the width as needed
+  }
 }
